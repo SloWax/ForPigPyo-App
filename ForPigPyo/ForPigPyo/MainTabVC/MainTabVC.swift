eed5c12abee5b0640a819ef108b38d291907d74f
SloWax <rjsnrl0128@naver.com>
2020-11-03 20:49:19 +0900

    feat: Pod install

---
+++ ForPigPyo/ForPigPyo/MainTabVC/MainTabVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabVC.swift
+//  ForPigPyo
+//
+//  Created by 표건욱 on 2020/11/03.
+//
+
+import UIKit
+
+class MainTabVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ ForPigPyo/Pods/Target Support Files/Pods-ForPigPyo/Pods-ForPigPyo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForPigPyo : NSObject
+@end
+@implementation PodsDummy_Pods_ForPigPyo
+@end
