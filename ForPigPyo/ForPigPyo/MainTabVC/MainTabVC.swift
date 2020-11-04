c6a9cfb17c7af4fb8460aad1ac44bee738eb72b9
SloWax <rjsnrl0128@naver.com>
2020-11-04 16:52:30 +0900

    feat: make Tabbar

--- ForPigPyo/ForPigPyo/MainTabVC/MainTabVC.swift
+++ ForPigPyo/ForPigPyo/MainTabVC/MainTabVC.swift
@@ -11,7 +11,25 @@ class MainTabVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        let homeVC = makeNaviController(tabBarImage: "house", rootViewController: HomeVC())
+        homeVC.title = "홈"
+        
+        
+        let myPageVC = makeNaviController(tabBarImage: "person", rootViewController: MyPageVC())
+        myPageVC.title = "마이페이지"
+        
+        viewControllers = [homeVC, myPageVC]
+        
+        tabBar.tintColor = .systemPurple
+    }
+    
+    func makeNaviController(tabBarImage: String, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.image = UIImage(systemName: tabBarImage)
+        navController.navigationBar.tintColor = .systemPurple
+        
+        return navController
     }
 }
